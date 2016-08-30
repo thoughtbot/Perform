b5a1bce0680a0853052fb6ea0a56c8c7f4668bec
Adam Sharp <adsharp@me.com>
2016-08-30 11:25:40 -0400

    Rename `configure` to `prepare`

--- Sources/Perform/Perform.swift
+++ Sources/Perform/Perform.swift
@@ -1,24 +1,24 @@
 import UIKit
 
 extension UIViewController {
-  public func perform<Destination: UIViewController>(segue: Segue<Destination>, configure: (Destination) -> Void) {
+  public func perform<Destination: UIViewController>(segue: Segue<Destination>, prepare: (Destination) -> Void) {
     performSegue(withIdentifier: segue.identifier) { segue, _ in
       guard let destination = segue.destinationViewController(ofType: Destination.self) else {
         let printHierarchy = "_printHierarchy"
         let hierarchy = segue.destinationViewController.performSelector(Selector(printHierarchy)).takeUnretainedValue()
         fatalError("expected destination view controller hierarchy to include \(Destination.self), got:\n\(hierarchy)")
       }
-      configure(destination)
+      prepare(destination)
     }
   }
 
-  internal func performSegue(withIdentifier identifier: String, sender: AnyObject? = nil, configure: (UIStoryboardSegue, AnyObject?) -> Void) {
+  internal func performSegue(withIdentifier identifier: String, sender: AnyObject? = nil, prepare: (UIStoryboardSegue, AnyObject?) -> Void) {
     try! aspect_hook(
       #selector(UIViewController.prepareForSegue(_:sender:)),
       options: [.PositionAfter, .OptionAutomaticRemoval],
       body: { info in
         let arguments = info.arguments()
-        configure(arguments.first as! UIStoryboardSegue, arguments.second)
+        prepare(arguments.first as! UIStoryboardSegue, arguments.second)
       }
     )
 
@@ -33,7 +33,7 @@ extension UIViewController {
   public func perform() {
     fatalError(
       "This method will never be called, and exists only to remove an " +
-      "apparent ambiguity resolving the generic method 'perform(_:configure:)'"
+      "apparent ambiguity resolving the generic method 'perform(_:prepare:)'"
     )
   }
 }
