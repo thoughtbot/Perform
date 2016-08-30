355952d46d01591296e848ff8c6fd8168ec032af
Adam Sharp <adsharp@me.com>
2016-08-30 16:12:12 -0400

    Add documentation comment for `configureViewControllers`

--- Sources/Perform/UITabBarController.swift
+++ Sources/Perform/UITabBarController.swift
@@ -1,6 +1,28 @@
 import UIKit
 
 extension UITabBarController {
+  /// For each view controller in `viewControllers`, iterate over its view
+  /// controller hierarchy, passing each view controller to `configure`.
+  ///
+  /// **Example**
+  ///
+  ///     class AppController: UITabBarController {
+  ///       let taskList = TaskList.shared
+  ///
+  ///       override func viewDidLoad() {
+  ///         super.viewDidLoad()
+  ///
+  ///         configureViewControllers { viewController in
+  ///           if let tasks = viewController as? TasksViewController {
+  ///             tasks.taskList = taskList
+  ///           }
+  ///         }
+  ///       }
+  ///     }
+  ///
+  /// - parameter configure:
+  ///     A function that will be invoked once with each view controller
+  ///     in each tab's view controller hierarchy.
   public func configureViewControllers(@noescape configure: (UIViewController) -> Void) {
     let hierarchy = (viewControllers ?? []).lazy.flatMap { $0.hierarchy }
     hierarchy.forEach(configure)
