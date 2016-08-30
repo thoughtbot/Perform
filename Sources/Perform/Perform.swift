f6f66f3b197f5816ad2bdd33bfeb4647649cd1cc
Adam Sharp <adsharp@me.com>
2016-08-30 16:52:41 -0400

    Add doc comment for `UIViewController.perform(_:prepare:)`

--- Sources/Perform/Perform.swift
+++ Sources/Perform/Perform.swift
@@ -1,6 +1,40 @@
 import UIKit
 
 extension UIViewController {
+  /// Perform the storyboard segue identified by `segue`, invoking `prepare`
+  /// with the destination view controller when `prepareForSegue(_:sender:)`
+  /// is received.
+  ///
+  /// **Example**
+  ///
+  ///     // prepare for segue inside the closure
+  ///     perform(.showTaskDetails) { taskVC in
+  ///       taskVC.task = self.selectedTask
+  ///     }
+  ///
+  ///     // just perform a segue
+  ///     perform(.signOut)
+  ///
+  /// If the destination view controller itself doesn't match the destination
+  /// type specified by `segue`, its view controller hierarchy is searched
+  /// until the matching view controller is found. For example, if the desired
+  /// view controller is embedded inside a `UINavigationController`, it will
+  /// still be found and passed along to `prepare`.
+  ///
+  /// - parameter segue:
+  ///     An instance of `Segue<Destination>` which specifies the identifier
+  ///     and destination view controller type.
+  ///
+  /// - parameter prepare:
+  ///     A function that will be invoked with the matching destination view
+  ///     view controller when `prepareForSegue(_:sender:)` is received. The
+  ///     default value for this parameter does nothing.
+  ///
+  /// - note:
+  ///     If no matching view controller is found in the destination
+  ///     view controller hierarchy, this method will raise a fatal error
+  ///     and crash. This usually means that the view controller hasn't
+  ///     been configured with the correct type in the storyboard.
   public func perform<Destination: UIViewController>(segue: Segue<Destination>, prepare: (Destination) -> Void = { _ in }) {
     performSegue(withIdentifier: segue.identifier) { segue, _ in
       guard let destination = segue.destinationViewController(ofType: Destination.self) else {
