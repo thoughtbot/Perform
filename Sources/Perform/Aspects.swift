ddc1b4d252e78f244b79e25d8b99615001957f1b
Adam Sharp <adam@sharplet.me>
2018-06-27 10:22:07 -0400

    Add missing @escaping annotation
    This should have been escaping all along, and fixes a compile error in Xcode 10.

--- Sources/Perform/Aspects.swift
+++ Sources/Perform/Aspects.swift
@@ -1,7 +1,7 @@
 import Aspects
 
 extension NSObject {
-  @nonobjc final func aspect_hook(_ selector: Selector, options: AspectOptions, body: @convention(block) (AspectInfo) -> Void) throws -> AspectToken {
+  @nonobjc final func aspect_hook(_ selector: Selector, options: AspectOptions, body: @convention(block) @escaping (AspectInfo) -> Void) throws -> AspectToken {
     return try self.aspect_hook(selector, with: options, usingBlock: unsafeBitCast(body, to: NSObject.self))
   }
 }
